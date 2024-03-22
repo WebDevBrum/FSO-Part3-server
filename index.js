9fd06071e9e77434eb631da06ca4c689777b8c25
Scott Davies <scottdavies1983@gmail.com>
2024-03-22 17:02:22 +0000

    3.2 html and date

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const numPersons = persons.length;
+  const now = new Date();
+  const html = `<p>Phonebook has info for ${numPersons} people</p><p>${now}</p>`;
+
+  response.send(html);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
