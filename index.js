8b8e15e0d010b1ccdc0179b0683c4215b0f4f4c5
Scott Davies <scottdavies1983@gmail.com>
2024-04-29 16:57:03 +0100

    added delete

--- index.js
+++ index.js
@@ -121,10 +121,17 @@ app.post("/api/persons", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+  const id = request.params.id;
 
-  response.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+
+  // persons = persons.filter((person) => person.id !== id);
+
+  // response.status(204).end();
 });
 
 const PORT = process.env.PORT || 3001;
