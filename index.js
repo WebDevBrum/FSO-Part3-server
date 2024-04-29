2aaa6c3ab9f952f50b0f6ddcf8e9b73e45de5e81
Scott Davies <scottdavies1983@gmail.com>
2024-04-29 17:15:31 +0100

    completed 3.16

--- index.js
+++ index.js
@@ -49,22 +49,19 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-  // const id = Number(request.params.id);
-  // const person = persons.find((person) => person.id === id);
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   response.status(404).end();
-  // }
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -85,7 +82,7 @@ const generateId = () => {
   return uniqueId;
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.number || !body.name) {
@@ -100,27 +97,20 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  // const person = {
-  //   id: generateId(),
-  //   name: body.name,
-  //   number: body.number,
-  // };
-
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
-
-  // persons = persons.concat(person);
-
-  // response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
   Person.findByIdAndDelete(id)
@@ -128,11 +118,24 @@ app.delete("/api/persons/:id", (request, response) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
-
-  // persons = persons.filter((person) => person.id !== id);
-
-  // response.status(204).end();
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).json({ error: "Malformatted ID" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else {
+    return response.status(500).json({ error: "Internal server error" });
+  }
+
+  // next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
